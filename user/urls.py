dc3449f39d806cd3f12ce0521938802b753a42b7
Leulseged-Mesfin <leulsegedmesfin16@gmail.com>
2025-01-10 20:31:30 +0300

    fixed some bugs.

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,6 @@ from .views import (
 urlpatterns = [
     path('', UserListCreateAPIView.as_view()),
     path('<pk>', UserRetrieveUpdateDeleteAPIView.as_view()),
-    path('profile', UserProfileView.as_view()),
+    path('profile/', UserProfileView.as_view()),
     path('change_password', UserChangePassword.as_view()),
 ]
